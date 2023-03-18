454aa452dcc095977bbc35ea40079b29f5a3a811
sanhendrindp <sanhendrin.dwip@gmail.com>
2023-03-18 15:10:57 +0700

    scripting javascript

---
+++ script.js
@@ -0,0 +1,62 @@
+'use strict';
+
+// Starting condition
+let score = 20;
+let highscore = 0;
+
+// Gain secret number
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+// console.log(secretNumber);
+
+// Function to display message
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+// Check button event
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // If there is no guess
+  if (!guess) {
+    displayMessage('⛔ No Number!');
+
+    // If guess is correct
+  } else if (guess === secretNumber) {
+    displayMessage("🎉 You're Correct!");
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '25rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // If guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.score').textContent = 0;
+      displayMessage('👎 You Lose...');
+      document.querySelector('body').style.backgroundColor = '#EA5455';
+    }
+  }
+});
+
+// Retry button event
+document.querySelector('.retry').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  //   console.log(secretNumber);
+  score = 20;
+
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.highscore').textContent = highscore;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = 'rgb(60, 60, 60)';
+  document.querySelector('.guess').value = '';
+  displayMessage('Start Guessing...');
+});
